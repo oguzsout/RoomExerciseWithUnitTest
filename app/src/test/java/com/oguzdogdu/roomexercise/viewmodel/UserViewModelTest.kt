243d924f3636f282080019109db32ecd053ec015
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-20 17:36:21 +0300

    First test passed

--- app/src/test/java/com/oguzdogdu/roomexercise/viewmodel/UserViewModelTest.kt
+++ app/src/test/java/com/oguzdogdu/roomexercise/viewmodel/UserViewModelTest.kt
@@ -1,4 +1,38 @@
 package com.oguzdogdu.roomexercise.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth
+import com.oguzdogdu.roomexercise.MainCoroutineRules
+import com.oguzdogdu.roomexercise.getOrAwaitValueTest
+import com.oguzdogdu.roomexercise.repo.FakeUserRepository
+import com.oguzdogdu.roomexercise.util.Status
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
 class UserViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRules()
+
+    private lateinit var viewModel: UserViewModel
+
+    @Before
+    fun setup() {
+        viewModel = UserViewModel(FakeUserRepository())
+    }
+    @Test
+    fun `insert user without age returns error`() {
+        viewModel.makeUser("Oguz", "Dogdu", "")
+
+        val value = viewModel.insertUserMessage.getOrAwaitValueTest()
+
+        Truth.assertThat(value.status).isEqualTo(Status.ERROR)
+    }
 }
