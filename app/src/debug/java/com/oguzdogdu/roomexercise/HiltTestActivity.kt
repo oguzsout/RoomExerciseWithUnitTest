d49cd72510dc56863e6f46d4ac104e8123bb3706
oguzsout <oguzd494@gmail.com>
2022-01-28 18:07:44 +0300

    preparing for instrumented tests

---
+++ app/src/debug/java/com/oguzdogdu/roomexercise/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.oguzdogdu.roomexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
